6f683d97183da67f2995b5256deab1c0bed659f6
beomsun0829 <ghsqjatns@gmail.com>
2024-10-21 10:09:24 +0900

    scan thermal_zone directory and find out cpu temp

--- module/cpu.cpp
+++ module/cpu.cpp
@@ -1,12 +1,15 @@
-#include "cpu.hpp"
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <unistd.h>
+#include <filesystem>
+#include "cpu.hpp"
+using namespace std;
 
 float CPU::getUsage() {
-    std::ifstream file("/proc/stat");
-    std::string line;
-    std::getline(file, line);
+    ifstream file("/proc/stat");
+    string line;
+    getline(file, line);
 
     unsigned long long user, nice, system, idle, iowait, irq, softirq, steal;
     sscanf(line.c_str(), "cpu  %llu %llu %llu %llu %llu %llu %llu %llu",
@@ -26,21 +29,55 @@ float CPU::getUsage() {
 }
 
 float CPU::getTemperature() {
-    std::ifstream file("/sys/class/thermal/thermal_zone0/temp");
-    int temp_milli;
-    file >> temp_milli;
-    return temp_milli / 1000.0;
+	string base_path = "/sys/class/thermal/";
+	string temp_path;
+
+	bool found = false;
+	for (const auto& entry : filesystem::directory_iterator(base_path)){
+		if (entry.is_directory() && entry.path().filename().string().find("thermal_zone") != string::npos){
+			string type_path = entry.path().string() + "/type";
+			ifstream typeFile(type_path);
+
+			if (!typeFile.is_open()){
+				continue;
+			}
+			
+			string type;
+			getline(typeFile, type);
+			
+			if (type == "x86_pkg_temp"){
+				temp_path = entry.path().string() + "/temp";
+				found = true;
+				break;
+			}
+		}
+	}
+	
+	if (!found){
+		return 0.0f;
+	}
+	
+	ifstream tempFile(temp_path);
+	if(!tempFile.is_open()){
+		return 0.0f;
+	}
+	
+	int temp_milli;
+	tempFile >> temp_milli;
+	
+	return temp_milli / 1000.0f;
+	
 }
 
 float CPU::getClockSpeed() {
-    std::ifstream file("/proc/cpuinfo");
-    std::string line;
+    ifstream file("/proc/cpuinfo");
+    string line;
 
-    while (std::getline(file, line)) {
-        if (line.find("cpu MHz") != std::string::npos) {
+    while (getline(file, line)) {
+        if (line.find("cpu MHz") != string::npos) {
             float clock_speed;
             sscanf(line.c_str(), "cpu MHz : %f", &clock_speed);
-            return clock_speed / 1000.0f;  // Convert MHz to GHz
+            return clock_speed / 1000.0f;
         }
     }
 
